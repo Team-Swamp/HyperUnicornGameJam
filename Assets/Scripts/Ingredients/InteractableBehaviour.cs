d6626bd1abc2196f660bc941cb122c2eb0fd9f7b
DaanDuits <90894914+DaanDuits@users.noreply.github.com>
2023-11-16 15:03:24 +0100

    Added more feedback

--- Assets/Scripts/Ingredients/InteractableBehaviour.cs
+++ Assets/Scripts/Ingredients/InteractableBehaviour.cs
@@ -1,34 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class InteractableBehaviour : MonoBehaviour
+namespace Ingredients
 {
-    [SerializeField] private GameObject hoverObject;
-    private GameObject hoverObjectInstance;
-    private Transform worldCanvas => GameObject.Find("WorldCanvas").transform;
-    private Transform player => GameObject.Find("PlayerObject").transform;
-
-    // Start is called before the first frame update
-    void Start()
+    public class InteractableBehaviour : MonoBehaviour
     {
-        hoverObjectInstance = Instantiate(hoverObject, transform.position, Quaternion.identity, worldCanvas);
-        hoverObjectInstance.transform.localScale = transform.localScale * 2;
-        hoverObjectInstance.SetActive(false);
-    }
+        [SerializeField] private GameObject hoverObject;
+        private GameObject _hoverObjectInstance;
+        private Transform WorldCanvas => GameObject.Find("WorldCanvas").transform;
+        private Transform Player => GameObject.Find("PlayerObject").transform;
 
-    void Update()
-    {
-        if (hoverObjectInstance.activeInHierarchy && Input.GetKeyDown(KeyCode.E))
-            Debug.Log("Collected");
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            _hoverObjectInstance = Instantiate(hoverObject, transform.position, Quaternion.identity, WorldCanvas);
+            _hoverObjectInstance.transform.localScale = transform.localScale * 2;
+            _hoverObjectInstance.SetActive(false);
+        }
 
-    void OnMouseEnter()
-    {
-        if ((transform.position - player.position).magnitude < 2) 
-            hoverObjectInstance.SetActive(true);
-    }
+        void Update()
+        {
+            if (_hoverObjectInstance.activeInHierarchy && Input.GetKeyDown(KeyCode.E))
+            {
+                // NOTE: add interaction
+            }
+        }
 
-    void OnMouseExit() => hoverObjectInstance.SetActive(false);
+        void OnMouseEnter()
+        {
+            if ((transform.position - Player.position).magnitude < 2) 
+                _hoverObjectInstance.SetActive(true);
+        }
+
+        void OnMouseExit() => _hoverObjectInstance.SetActive(false);
+    }
 }
--- Assets/Scripts/UI/LookAtCamera.cs
+++ Assets/Scripts/UI/LookAtCamera.cs
@@ -1,12 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class LookAtCamera : MonoBehaviour
+namespace UI
 {
-    void LateUpdate()
+    public class LookAtCamera : MonoBehaviour
     {
-        transform.LookAt(Camera.main.transform);
-        transform.Rotate(0, 180, 0);
+        void LateUpdate()
+        {
+            transform.LookAt(Camera.main.transform);
+            transform.Rotate(0, 180, 0);
+        }
     }
 }
